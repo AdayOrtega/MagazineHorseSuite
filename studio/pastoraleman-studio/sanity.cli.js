70449147be16d816b6161b81ee51318dfc3fe24d
Aday Ortega <aday.ortega@gmail.com>
2026-02-22 17:58:08 +0000

    chore(studio): add deployment appId

--- studio/pastoraleman-studio/sanity.cli.js
+++ studio/pastoraleman-studio/sanity.cli.js
@@ -6,6 +6,8 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'm46fo6ysi7xsycwu8tc572v2',
+
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
